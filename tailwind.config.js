eb5afa2a9b548fd6b5e0ddde24cd7268cd69e346
lkkolosowski <lkkolosowski@gmail.com>
2023-05-21 21:23:12 +0200

    add custom animation to globals.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,13 @@ module.exports = {
         blinking: {
           to: { visibility: "hidden" },
         },
+        pulsing: {
+          "50%": { opacity: 0.3 },
+        },
       },
       animation: {
         blinking: "blinking 1s steps(2, start) infinite",
+        pulsing: "pulsing 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
       },
     },
   },
